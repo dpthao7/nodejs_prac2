4718fef3a6de4cb6ea54d05c562fdb982a752fca
thaodp7 <greenbean0889@gmail.com>
2022-04-21 21:17:07 +0700

    remove public/css

---
+++ src/index.js
@@ -0,0 +1,39 @@
+//Loads the express module
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+//Creates express server
+const app = express();
+
+const port = 3000
+
+//Serves static files (file tĩnh trong thư mực public)
+app.use(express.static(path.join(__dirname,'public')));
+
+// Http logger
+app.use(morgan('combined'));
+
+// Template engine
+// app.engine('handlebars', handlebars() ); // Lỗi, phải dùng code 2 dòng dưới
+const hbs = handlebars.create({defaultLayout: 'main'});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views',path.join(__dirname, 'resources/views'))
+
+// console.log(path.join(__dirname, 'views'));
+
+// app.get('/', (req, res) => res.send('Hello World'))
+
+app.get('/', (req, res) => {
+    // Render ra nội dung từ home.handlebars
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    // Render ra nội dung từ home.handlebars. Header, Footer là tĩnh, ko đổi, nội dung body thay đổi
+    res.render('news');
+});
+
+app.listen(port, () => console.log(`Example all listening at http://localhost:${port}`))
